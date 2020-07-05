a2842f8a64517c5a1e682fd8ef3a19412f0b9134
Pedro Henrique Sena Lima <62624302+phslima@users.noreply.github.com>
2020-07-05 18:00:17 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,36 @@
+
+
+
+
+function setup() {
+  
+  frameRate(40);
+  jogo=new Jogo();
+  telaInicial=new TelaInicial();
+  jogo.setup()
+   cenas={
+     jogo,
+     telaInicial
+   };
+  botaoGerenciador=new BotaoGerenciador('Iniciar',width/2,height/2);
+  
+}
+
+function keyPressed(){
+  jogo.keyPressed(key)
+  
+}
+
+function draw() {
+  
+  cenas[cenaAtual].draw();
+  
+    
+}
+
+
+
+
+
+
+
